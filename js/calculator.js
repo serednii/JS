666c1e7cf4a470218e809547ef074545e4b2108d
Serednii Mykola <seredniimykola@gmail.com>
2024-02-13 17:23:15 +0100

    add calculator

---
+++ js/calculator.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+//---------------------------------------------------
+
+
+var equation = '3+2+5*4/32*14+4587';
+var operators = equation.split(/[0-9]+/).filter(Boolean); // Extract operators
+var numbers = equation.split(/[\+\-\*\/]/).filter(Boolean); // Extract numbers
+var equationArray = [];
+// Push the first number to the array
+equationArray.push(parseFloat(numbers[0]));
+// Loop through the operators and numbers and push them alternately into the array
+
+for (var i = 0; i < operators.length; i++) {
+    equationArray.push(operators[i]);
+    equationArray.push(parseFloat(numbers[i + 1]));
+}
+
+console.log(equationArray);
+console.log(numbers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/index.js
+++ js/index.js
@@ -1,3 +1,1 @@
-
-
-
+import './calculator.js';
